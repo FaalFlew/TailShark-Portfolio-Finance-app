d280fe671844e55e48d73c54074dc9a5028f9386
Majd Abbas <126261797+FaalFlew@users.noreply.github.com>
2024-06-23 13:21:03 +0200

    closes #2, added delete functionality to delete tickers from portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import { useState, ChangeEvent, SyntheticEvent } from 'react';
 import { CompanySearch } from './comapny';
 import { searchCompanies } from './api';
@@ -9,6 +10,7 @@ import { searchCompanies } from './api';
 function App() {
 
   const [search,setSearch] = useState<string>('');
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) =>
@@ -31,15 +33,27 @@ function App() {
       console.log("lol",setSearchResult);
   };
   
-  const onPortfolioCreate = (e: SyntheticEvent) =>
+  const onPortfolioCreate = (e: any) =>
   {
     e.preventDefault();
-    console.log(e);
+    const exists = portfolioValues.find((value) => value === e.target[0].value)
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
   };
+
+  const onPortfolioDelete = (e:any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter( (value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  }
 
   return (
     <div className="App">
       <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
+      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
       <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
       {/*serverError && <h1>{serverError}</h1>*/}
     </div>
