822f4cef3bddd9c644a89f90719e10c024b01dad
Majd Abbas <126261797+FaalFlew@users.noreply.github.com>
2024-08-14 00:34:38 +0200

    closes #45, improved & optimized sidebar, adjusted some css on a few components

--- frontend/src/Components/SideBar/SideBar.tsx
+++ frontend/src/Components/SideBar/SideBar.tsx
@@ -1,36 +1,83 @@
-// Sidebar.tsx
-
-import React, { useState } from 'react';
-import SidebarItem from './SideBarItem/SideBarItem';
-import 'boxicons/css/boxicons.min.css';
+import React, { useEffect, useRef, useState } from "react";
+import SidebarItem from "./SideBarItem/SideBarItem";
+import "boxicons/css/boxicons.min.css";
 import "./SideBar.css";
 
 const Sidebar: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const sidebarRef = useRef<HTMLElement>(null);
 
   const handleResizeClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     setIsExpanded((prevExpanded) => !prevExpanded);
-    document.body.classList.toggle('sb-expanded');
+    document.body.classList.toggle("sb-expanded");
   };
 
+  const handleLinkClick = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      document.body.classList.remove("sb-expanded");
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node) &&
+        isExpanded
+      ) {
+        setIsExpanded(false);
+        document.body.classList.remove("sb-expanded");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isExpanded]);
+
   return (
-      <aside className={`sidebar-container ${isExpanded ? 'sb-expanded' : ''}`}>
-        <nav>
-          <ul>
-            <SidebarItem to="profile" icon="bx bx-home-circle" label="Profile" />
-            <SidebarItem to="income" icon="bx bx-money" label="Income" />
-            <SidebarItem to="balance" icon="bx bx-spreadsheet" label="Balance" />
-            <SidebarItem to="cashflow" icon="bx bx-dollar-circle" label="Cashflow" />
-            <li>
-              <a href="#" onClick={handleResizeClick} data-resize-btn>
-                <i className="bx bx-chevrons-right"></i>
-                <span>Collapse</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </aside>
+    <aside
+      ref={sidebarRef}
+      className={`sidebar-container ${isExpanded ? "sb-expanded" : ""}`}
+    >
+      <nav className="side-nav">
+        <ul>
+          <SidebarItem
+            to="profile"
+            icon="bx bx-home-circle"
+            label="Profile"
+            onClick={handleLinkClick}
+          />
+          <SidebarItem
+            to="income"
+            icon="bx bx-money"
+            label="Income"
+            onClick={handleLinkClick}
+          />
+          <SidebarItem
+            to="balance"
+            icon="bx bx-spreadsheet"
+            label="Balance"
+            onClick={handleLinkClick}
+          />
+          <SidebarItem
+            to="cashflow"
+            icon="bx bx-dollar-circle"
+            label="Cashflow"
+            onClick={handleLinkClick}
+          />
+          <li>
+            <a href="#" onClick={handleResizeClick} data-resize-btn>
+              <i className="bx bx-chevrons-right"></i>
+              <span>Collapse</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
   );
 };
 
--- frontend/src/Components/SideBar/SideBarItem/SideBarItem.tsx
+++ frontend/src/Components/SideBar/SideBarItem/SideBarItem.tsx
@@ -5,16 +5,18 @@ interface SidebarItemProps {
   to: string;
   icon: string;
   label: string;
+  onClick?: () => void;
+
 }
 
-const SideBarItem: React.FC<SidebarItemProps> = ({ to, icon, label }) => {
+const SideBarItem: React.FC<SidebarItemProps> = ({ to, icon, label, onClick}) => {
   const location = useLocation();
 
   const isActiveLink = (path: string) => location.pathname.includes(path);
 
   return (
     <li>
-      <Link to={to} className={isActiveLink(to) ? 'active' : ''}>
+      <Link to={to} className={isActiveLink(to) ? 'active' : ''} onClick={onClick}>
         <i className={icon}></i>
         <span>{label}</span>
       </Link>
