1324ac976328e48290a89fa25bc16358c4a0945a
Majd Abbas <126261797+FaalFlew@users.noreply.github.com>
2025-05-25 22:59:19 +0200

    Added FluentValidation for stock and comment system.

---
+++ api/Validators/Comment/CreateCommentValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using api.Dtos.Comment;
+
+namespace api.Validators.Comment
+{
+    public class CreateCommentValidator : AbstractValidator<CreateCommentDto>
+    {
+        public CreateCommentValidator()
+        {
+            RuleFor(comment => comment.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MinimumLength(5).WithMessage("Title has to be at least 5 characters.")
+                .MaximumLength(60).WithMessage("Title can be at most 60 characters.");
+
+            RuleFor(comment => comment.Content)
+                .NotEmpty().WithMessage("Message is required.")
+                .MinimumLength(5).WithMessage("Message has to be at least 5 characters.")
+                .MaximumLength(280).WithMessage("Message can be at most 280 characters.");
+        }
+    }
+}
---
+++ api/Validators/Stock/CreateStockValidator.cs
@@ -0,0 +1,28 @@
+using FluentValidation;
+using api.Dtos.Stock;
+
+namespace api.Validators.Stock
+{
+    public class CreateStockValidator : AbstractValidator<CreateStockRequestDto>
+    {
+        public CreateStockValidator()
+        {
+            RuleFor(stock => stock.Symbol)
+                    .NotEmpty().WithMessage("Symbol is required.")
+                    .MaximumLength(10).WithMessage("Symbol can be at most 10 characters.");
+
+            RuleFor(stock => stock.CompanyName)
+                    .NotEmpty().WithMessage("Company name is required.")
+                    .MaximumLength(20).WithMessage("Company name can be at most 20 characters.");
+
+            RuleFor(x => x.Purchase)
+                    .GreaterThanOrEqualTo(1).WithMessage("Purchase must be at least 1")
+                    .LessThanOrEqualTo(1_000_000_000).WithMessage("Purchase must be less than or equal to 1,000,000,000");
+
+            RuleFor(x => x.MarketCap)
+                    .GreaterThanOrEqualTo(1).WithMessage("Market Cap must be at least 1")
+                    .LessThanOrEqualTo(10_000_000_000_000).WithMessage("Market Cap must be less than or equal to 10,000,000,000,000");
+
+        }
+    }
+}
